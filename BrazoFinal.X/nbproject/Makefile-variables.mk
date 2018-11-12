eec036934d5ee75f0af6a0f89df8a5a02c209bf4
Sang Woo Shin <sangwooze@gmail.com>
2018-11-11 21:14:02 -0600

    added project

---
+++ BrazoFinal.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=BrazoFinal.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/BrazoFinal.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=brazofinal.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/brazofinal.x.tar
